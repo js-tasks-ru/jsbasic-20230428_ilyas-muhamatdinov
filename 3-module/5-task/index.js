1d8e0ffe0d5091ad0439daaf32148ae6ec6f5536
Ilyas Muhamatdinov <ilyas_muhamatdinov@mail.ru>
2023-05-19 19:31:27 +0400

    completed 3 modules

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbersArray = str.split(' ')
+  .filter(elem => {
+    return !isNaN(parseFloat(elem)) && isFinite(elem);
+  })
+  .map(elem => parseFloat(elem));
+
+  const result = {
+    min: Math.min(...numbersArray),
+    max: Math.max(...numbersArray)
+  };
+
+  return result;
 }
